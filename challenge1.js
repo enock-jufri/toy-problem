0e2f3e56937b8c1cbbf5c9737a5c19840117515d
enock-jufri <enockjufri@gmail.com>
2024-10-26 07:48:38 +0300

    grades student marks using a funtion and iteration loop

--- challenge1.js
+++ challenge1.js
@@ -1,22 +1,46 @@
-const students = {}
-process.stdout.write('Enter stduent\'s name and their marks in the format of name marks, enter done to exit: ');
+const students = {};
+process.stdout.write(
+  "Enter stduent's name and their marks in the format of name marks, enter done to exit: "
+);
 
-process.stdin.on('data', (data)=>{
-    const input = data.toString().trim()
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
 
-    if(input.toLowerCase() === 'done'){
-        process.stdin.pause()
-        return
+  if (input.toLowerCase() === "done") {
+    process.stdin.pause();
+
+    console.log("NAME   MARKS   GRADE")
+
+    for (const key in students) {
+      const grade = grading(students[key]);
+      console.log(`${key}${students[key]}   ${grade}`);
     }
-    
-    const [name, marksStr]= input.split(' ')
+    return;
+  }
 
-    let  marks = parseInt(marksStr)
+  const [unpadedName, marksStr] = input.split(" ");
 
-    if(isNaN(marks) || marks < 0 || marks > 100){
-        console.log('invalid number.Enter name and marks again');
-        return;
-    }
+  let marks = parseInt(marksStr);
+  let name = unpadedName.padEnd(8, ' ')
 
-    students[name] = marks
-})
+  if (isNaN(marks) || marks < 0 || marks > 100) {
+    console.log("invalid number.Enter name and marks again");
+    return;
+  }
+
+  students[name] = marks;
+});
+
+function grading(mark) {
+  if (mark > 79) {
+    return "A";
+  } else if (mark >= 60 && mark <= 79) {
+    return "A-";
+  } else if (mark >= 50 && mark <= 59) {
+    return "B+";
+  } else if (mark >= 40 && mark <= 49) {
+    return "B";
+  } else {
+    return "C";
+  }
+}
